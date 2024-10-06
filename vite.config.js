a007e7a8b1945ac8496e437d832c74fb98a5009d
Anik <deyanik659@gmail.com>
2024-10-06 14:25:31 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/React-Calculator/',
   plugins: [react()],
   build: {
     outDir: 'build', // Change the output directory from 'dist' to 'build'
